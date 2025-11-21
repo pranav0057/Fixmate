dead12ca0c4fd0bc872dea806c28cbb93a680fc3
Pranav Chaudhary <195259661+pranav0057@users.noreply.github.com>
2025-11-22 03:28:04 +0530

    fine

--- codeeditor-backend/src/utils/sendEmail.js
+++ codeeditor-backend/src/utils/sendEmail.js
@@ -3,15 +3,27 @@ import nodemailer from "nodemailer";
 
 export const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    service: "Gmail",
-    host: "smtp.gmail.com",  
     port: 465,
+    host: "smtp.gmail.com",  
     secure: true,  
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+    secure:true,
   }); 
+
+  await new Promise((resolve, reject) => {
+    transporter.verify(function (error, success) {
+        if (error) {
+            console.log(error);
+            reject(error);
+        } else {
+            console.log("Server is ready to take our messages");
+            resolve(success);
+        }
+    });
+});
 
   const mailOptions = {
     from: `FixMate Support <${process.env.EMAIL_USER}>`,
@@ -20,7 +32,18 @@ export const sendEmail = async (options) => {
     html: options.message,
   };
 
-  await transporter.sendMail(mailOptions);
+  await new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            console.error(err);
+            reject(err);
+        } else {
+            console.log(info);
+            resolve(info);
+        }
+    });
+});
+  
 };
 
 
