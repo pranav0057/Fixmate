e02db13a98531b205878c42bd7bfbd3caa5c8760
Pranav Chaudhary <195259661+pranav0057@users.noreply.github.com>
2025-11-22 02:01:34 +0530

    frustrated-more and more

--- codeeditor-backend/src/utils/sendEmail.js
+++ codeeditor-backend/src/utils/sendEmail.js
@@ -1,19 +1,16 @@
 // src/utils/sendEmail.js
 import nodemailer from "nodemailer";
+
 export const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    service: "Gmail",  // Gmail is used as the SMTP service
+    service: "Gmail",
     host: "smtp.gmail.com",  // SMTP server address for Gmail
-    port: 587,  // Port for TLS
-    secure: false,  // Set to false for TLS (true for SSL)
+    secure: true,  // Set to false for TLS (true for SSL)
     auth: {
-      user: process.env.EMAIL_USER,  // Your Gmail email
-      pass: process.env.EMAIL_PASS,  // Your App Password
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
     },
-    tls: {
-      rejectUnauthorized: false,  // Allow to bypass unauthorized certificates (if needed)
-    },
-  });
+  }); 
 
   const mailOptions = {
     from: `FixMate Support <${process.env.EMAIL_USER}>`,
