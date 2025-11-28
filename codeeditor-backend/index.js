26cb4e6676436bcd5df2814ebd317cb66bfe7897
Pranav Chaudhary <195259661+pranav0057@users.noreply.github.com>
2025-11-28 22:51:37 +0530

    final try again

--- codeeditor-backend/index.js
+++ codeeditor-backend/index.js
@@ -17,7 +17,7 @@ initSocket(server);
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://fixmate-code.vercel.app",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
